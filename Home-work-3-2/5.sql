10b0065660d2ccb31410b0674181c0fb903bf4fb
Vadym Korolov <60427025+korolov-vv@users.noreply.github.com>
2021-05-21 01:52:52 +0200

    fix 5.sql

--- Home-work-3-2/5.sql
+++ Home-work-3-2/5.sql
@@ -7,10 +7,12 @@
 		INNER JOIN developers_on_projects AS dp ON p.project_id = dp.project_id
 		INNER JOIN developers AS d ON d.developer_id = dp.developer_id
 		GROUP BY p.project_id
-	) as min_coast_table
+	)as min_coast_table
 	on min_coast_table.project_id = p.project_id
 	WHERE coast = 
 	(
 		SELECT MIN(coast)
-		FROM projects
+		FROM projects p
+		INNER JOIN developers_on_projects AS dp ON p.project_id = dp.project_id
+		INNER JOIN developers AS d ON d.developer_id = dp.developer_id
 	);
